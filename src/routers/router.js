8514a82a4f0260dd9a05097ac800e966de6a0338
adarsh <jhaa50872@gmail.com>
2024-07-31 19:37:52 +0530

    completed

--- src/routers/router.js
+++ src/routers/router.js
@@ -15,4 +15,55 @@ router.post('/batsmen', async (req, res) =>{
         res.status(404).send('something went wrong')
     }
 })
+router.get('/batsmen', async (req, res) =>{
+    try {
+        let findPlayer = await Batter.find()
+        console.log(findPlayer, 'get request successful🚩');
+        res.status(201).send(findPlayer)
+    } catch (error) {
+        console.log(error);
+        res.status(404).send('something went wrong')
+    }
+})
+router.get('/batsmen/:id', async (req, res) =>{
+    try {
+        let id = req.params.id
+        let getById =  Batter.findById(id)
+        let data = await getById
+        if (data === null) {
+           return res.send("enter valid id, this id doesn't exists")
+        }
+        console.log(data, 'getById request successful🚩');
+        res.status(201).send(data)
+    } catch (error) {
+        console.log(error);
+        res.status(404).send('something went wrong')
+    }
+})
+router.put('/batsmen/:id', async (req, res) =>{
+    try {
+        let id = req.params.id
+        let updatePlayer = Batter.findByIdAndUpdate(id, req.body, {new: true})
+        let data = await updatePlayer
+        console.log(data, 'put request successful🚩');
+        res.status(201).send(data)
+    } catch (error) {
+        console.log(error);
+        res.status(404).send('something went wrong')
+    }
+})
+router.delete('/batsmen/:id', async (req, res) =>{
+    try {
+        let id = req.params.id
+        let deletePlayer = Batter.findByIdAndDelete(id)
+        let data = await deletePlayer
+        console.log(data, 'delete request successful🚩');
+        res.status(201).send(data)
+    } catch (error) {
+        console.log(error);
+        res.status(404).send('something went wrong')
+    }
+})
+
+
 module.exports = router
